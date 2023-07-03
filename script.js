5856fd6e640f3b96bf13def36cbf286439e8945e
RamyaShetty81 <120573688+RamyaShetty81@users.noreply.github.com>
2023-07-03 14:23:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,12 +18,13 @@ const library = [
 
 const numberOfBooksRead = () => {
   // write your code here
-	let count = 0;
-	for(int i = 0;i<library.length;i++)
-	{
-		if(library[i].readingStatus) count++;	
-	}
-	return count;
+	 var count = 0;
+  for (var i = 0; i < library.length; i++) {
+    if (library[i].readingStatus) {
+      count++;
+    }
+  }
+  return count;
 };
 
 // Do not change the code below
